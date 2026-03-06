09ec89ecf1b6e311ffdca6ca37146fd6e04359fc
romanrac2r <roman.rac@growclub.sk>
2026-03-06 16:43:08 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: 'Forbes Growclub Assessment',
+  description: 'Spiral Dynamics Assessment',
+}
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="sk">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  )
+}
