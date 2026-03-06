8a536c6cc6a487f8013a5a59498953c80fb153d7
romanrac2r <roman.rac@growclub.sk>
2026-03-06 16:49:41 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 export const metadata = {
   title: 'Forbes Growclub Assessment',
-  description: 'Assessment na hodnotové rámce',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="sk">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <body style={{ margin: 0, padding: 0, fontFamily: 'sans-serif' }}>{children}</body>
     </html>
   )
 }
