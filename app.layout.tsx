b4fd6e845de06943f17a2c05b739bfb9d3b0ffec
romanrac2r <roman.rac@growclub.sk>
2026-03-06 16:58:01 +0100

    Create app.layout.tsx

---
+++ app.layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Forbes Growclub Assessment' }
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="sk">
+      <body style={{ margin: 0, padding: 0, fontFamily: 'sans-serif' }}>{children}</body>
+    </html>
+  )
+}
